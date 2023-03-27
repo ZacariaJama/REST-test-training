5c88a177bad351b02ed125fb60577bed4317f41c
Zacaria Jama <zjama@uwindsor.ca>
2023-03-27 14:58:58 -0400

    commit 3

--- src/main/java/com/example/KeyPrototype/controller/BuildingController.java
+++ src/main/java/com/example/KeyPrototype/controller/BuildingController.java
@@ -1,9 +1,14 @@
 package com.example.KeyPrototype.controller;
 
+import com.example.KeyPrototype.models.Building;
 import com.example.KeyPrototype.repository.BuildingRepository;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 public class BuildingController {
@@ -17,5 +22,11 @@ public class BuildingController {
     public String getBuilding(Model model) {
         model.addAttribute("buildings", buildingRepository.findAll());
         return "buildings";
+    }
+
+    @GetMapping("/buildings/")
+    ResponseEntity<List<Building>> getBuildings() {
+        Iterable<Building> buildings = buildingRepository.findAll();
+        return ResponseEntity.ok((List<Building>) buildings);
     }
 }
--- src/main/java/com/example/KeyPrototype/controller/DoorController.java
+++ src/main/java/com/example/KeyPrototype/controller/DoorController.java
@@ -1,9 +1,15 @@
 package com.example.KeyPrototype.controller;
 
+import com.example.KeyPrototype.models.Building;
+import com.example.KeyPrototype.models.Door;
 import com.example.KeyPrototype.repository.DoorRepository;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 public class DoorController {
@@ -17,5 +23,11 @@ public class DoorController {
     public String getDoors(Model model) {
         model.addAttribute("doors", doorRepository.findAll());
         return "doors";
+    }
+
+    @GetMapping("/doors/")
+    ResponseEntity<List<Door>> getBuildings() {
+        Iterable<Door> doors = doorRepository.findAll();
+        return ResponseEntity.ok((List<Door>) doors);
     }
 }
