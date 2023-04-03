7af5bc68a36283e3ca6a99afb461b86eb99b75d9
Zacaria Jama <zjama@uwindsor.ca>
2023-04-03 11:07:49 -0400

    quick patch

--- src/main/java/com/example/KeyPrototype/KeyPrototypeApplication.java
+++ src/main/java/com/example/KeyPrototype/KeyPrototypeApplication.java
@@ -2,6 +2,10 @@ package com.example.KeyPrototype;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
 
 @SpringBootApplication
 public class KeyPrototypeApplication {
@@ -10,4 +14,13 @@ public class KeyPrototypeApplication {
 		SpringApplication.run(KeyPrototypeApplication.class, args);
 	}
 
+	@Bean
+	SecurityFilterChain springSecurity(HttpSecurity http) throws Exception {
+		HttpSessionRequestCache requestCache = new HttpSessionRequestCache();
+		requestCache.setMatchingRequestParameterName("/");
+		http.requestCache((cache) -> cache
+				.requestCache(requestCache)
+			);
+		return http.build();
+	}
 }
